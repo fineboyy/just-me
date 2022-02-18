9a333504accb97e0f4f7970a23cda9be6b9438b7
fineboyy <maqweku@gmail.com>
2022-02-18 17:20:15 +0000

    Added Dynamic Layout for Posts

--- mvc/controllers/indexController.js
+++ mvc/controllers/indexController.js
@@ -1,7 +1,10 @@
+let postData = require('../../postData')
 
+postData = postData.postData
 
 const getHomePage = function(req, res) {
-    res.render("index", {title: "Just Me"})
+    console.log(postData.length)
+    res.render("index", {title: "Just Me", posts: postData})
 }
 
 
